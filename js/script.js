5af761e5be9ffc41fd02462ee4315c1b285cfa98
car-dan <73527175+car-dan@users.noreply.github.com>
2021-03-27 15:51:11 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -12,3 +12,81 @@ const cats = [
     }
 ];
 
+
+//question 1
+
+const cat = {
+    complain: function () {
+        console.log("Meow!");
+    }
+};
+
+
+
+//question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+//question 3
+
+heading.style.fontSize = "2em";
+
+//question 4
+
+heading.classList.add("subheading");
+
+//question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+};
+
+//question 6
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = `<p>New paragraph</p>`;
+resultsContainer.style.backgroundColor = "yellow";
+
+//question 7
+
+function animal(list) {
+    for (let i = 0; i < list.length; i++){
+        console.log(list[i].name);
+    }
+};
+
+animal(cats);
+
+
+//question 8
+
+function createCats(cats) {
+
+    let html = "";
+
+    for (let i = 0; i < cats.length; i++){
+        
+        let ageCats = "Age unknown";
+
+        if (cats[i].age) {
+            ageCats = cats[i].age;
+        }
+        
+        html += "<div>" + "<h5>" + cats[i].name + "</h5>" + "<p>" + ageCats+ "</p>" + "</div>";
+
+    }
+    return html;
+};
+
+const catContainer = document.querySelector(".cat-container");
+
+catContainer.innerHTML = createCats(cats);
+
+
+
+
